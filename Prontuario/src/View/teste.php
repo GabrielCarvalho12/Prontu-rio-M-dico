14ae54e9d00fcb4dfb12eac4a2b2e0f38f2f672c
Gabriel <gabriellcarvalho12@hotmail.com>
2017-09-23 23:40:33 -0300

    Tela de Login com Autenticação

---
+++ Prontuario/src/View/teste.php
@@ -0,0 +1,14 @@
+<?php
+require("../Model/seguranca.php"); // Inclui o arquivo com o sistema de segurança
+protegePagina(); // Chama a função que protege a página
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+teste
+</body>
+</html>
